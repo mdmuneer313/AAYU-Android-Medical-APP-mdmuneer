13958a76b841534177f1643c11681f1813f55b1d
muneer <jamesbondmuneer@gmail.com>
2020-03-24 17:14:06 +0530

    done

--- app/src/main/java/com/example/muneer/LoginFragment.java
+++ app/src/main/java/com/example/muneer/LoginFragment.java
@@ -8,12 +8,17 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 
 public class LoginFragment extends AppCompatActivity {
-    EditText fullname,password;
+    EditText email,password;
     Button loginBtn;
     ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_fragment);
+        email=findViewById(R.id.et_email);
+        password=findViewById(R.id.et_password);
+        loginBtn=findViewById(R.id.btn_login);
+        progressBar=findViewById(R.id.)
+
     }
 }
