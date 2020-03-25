59e0a2cbed217b0b906e38f8d40069c3e32eac69
muneer <jamesbondmuneer@gmail.com>
2020-03-25 21:07:54 +0530

    Merge remote-tracking branch 'Home/master'
    # Conflicts:
    #	app/src/main/java/com/example/muneer/LoginFragment.java
    #	app/src/main/res/layout/activity_registration_fragment.xml

--- app/src/main/java/com/example/muneer/Nav_Bottom.java
+++ app/src/main/java/com/example/muneer/Nav_Bottom.java
@@ -23,7 +23,7 @@ ActionBar actionBar;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav__bottom);
-        getSupportActionBar().setTitle("About");
+        getSupportActionBar().setTitle("Ayu");
         actionBar=getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#009688")));
         BottomNavigationView bottomNav = findViewById(R.id.nav_bottom);
