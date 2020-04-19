233ca89834ac89325fa648c6cc06824f5508964d
muneer <jamesbondmuneer@gmail.com>
2020-04-20 01:03:09 +0530

    done

--- app/src/main/java/com/example/muneer/AboutFragment.java
+++ app/src/main/java/com/example/muneer/AboutFragment.java
@@ -1,5 +1,7 @@
 package com.example.muneer;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,6 +18,8 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AboutFragment extends Fragment {
 CircleImageView androidstudio,firebase;
+CardView github;
+
     public AboutFragment() {
         // Required empty public constructor
     }
@@ -26,9 +31,19 @@ CircleImageView androidstudio,firebase;
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view= inflater.inflate(R.layout.fragment_about, container, false);
-androidstudio= view.findViewById(R.id.Androidstudio);
-firebase=view.findViewById(R.id.firebase);
-
+        github=view.findViewById(R.id.ggithublik);
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://github.com/mdmuneer313/muneer"));
+                startActivity(intent);
+            }
+        });
+//androidstudio= view.findViewById(R.id.Androidstudio);
+//firebase=view.findViewById(R.id.firebase);
 
         return view;
     }
